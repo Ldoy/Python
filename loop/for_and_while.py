ace27e767d549a8f562e12bda1cea41b7d5e46ce
Do Yi Lee <34529917+Ldoy@users.noreply.github.com>
2021-11-07 20:35:48 +0900

    Update for_and_while.py

--- loop/for_and_while.py
+++ loop/for_and_while.py
@@ -75,13 +75,3 @@ num = 0
 while num < 3:
   print('치킨', num, '마리')
   num = num + 1
-
-# input(), 항상 문자열로 입력을 받음
-name = input('이름뭐야?')
-print(name, '안녕')
-
-city_name = ''
-while city_name != '런던':
-  city_name = input('영국의 수도는?')
-print('정답입니다!')
-
