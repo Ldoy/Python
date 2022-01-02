83caa2873edaa4705e99c0f3d331bb38d8108665
Ldoy <leedoy7989@gmail.com>
2022-01-02 18:13:32 +0900

    feat: Add backgound setting codes and image

---
+++ Tacocat/pygame_basic/2_add_backGround.py
@@ -0,0 +1,36 @@
+##make pygame basic(뼈대)
+import pygame
+
+pygame.init() 
+
+# 화면 크기 설정
+
+screen_width = 480 
+screen_height = 640 
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀
+pygame.display.set_caption('Nado Game')
+
+background = pygame.image.load('/Users/joy/Projects/python_Exercise/Python/Tacocat/pygame_basic/background.jpg')
+
+# 동작검사하는 이벤트 루프가 실행되어야 창이 꺼지지 않음 
+running = True
+while running:
+    for event in pygame.event.get():
+        #사용자의 이벤트를 체크하는 것
+        if event.type == pygame.QUIT: 
+            # 창을 끄는 버튼을 누르면 해당 이벤트가 발생
+            running = False 
+                
+    screen.fill((0, 0, 255))
+
+    screen.blit(background, (20, 0))
+    ## while 문에서 계속 프레임을 그려주어야함 
+    pygame.display.update()
+
+# 게임 종료 시 처리 
+pygame.quit()
+
+# 배경 넣기 
+
