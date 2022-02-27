cda74897feed26ee3e7d0ccb40588e38bc686199
Ldoy <leedoy7989@gmail.com>
2022-02-27 18:25:15 +0900

    Add: Implement makeURL Method in Crawler Type

---
+++ Tacocat/Crawling/KeyboardCrawler/KeyboardCrawler/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  KeyboardCrawler
+//
+//  Created by Do Yi Lee on 2022/02/26.
+//
+
+import Foundation
+import SwiftSoup
+
+let url = "https://www.daangn.com/search/기계식%20키보드"
+
+enum CrawerError: Error {
+    case addressInvalid
+}
+
+class Crawer {
+    
+    // 1. url바꾸기
+    private func makeURL(url address: String) throws -> URL {
+        guard let url = URL(string: address) else {
+            print("url오류")
+            throw CrawerError.addressInvalid
+        }
+        return url
+    }
+    
+    //2. html로 바꾸고 SwiftSoup로 파싱하기
+}
